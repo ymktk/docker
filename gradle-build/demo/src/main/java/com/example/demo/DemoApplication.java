cb99849a46625244c41c4a11693b997dc1e29e65
yamakitakahiro <yamaki.takahiro@gmail.com>
2019-11-21 11:00:12 +0800

    Change text. Add README

--- gradle-build/demo/src/main/java/com/example/demo/DemoApplication.java
+++ gradle-build/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
   String index(){
-      return "Hello World!";
+      return "Hello Gradle!";
   }
 
 	public static void main(String[] args) {
